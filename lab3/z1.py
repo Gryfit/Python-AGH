44148ef36ca3b3a3b89987e9462a1bd68eb29bcb
Gryfit <bartektonia@gmail.com>
2018-04-30 17:22:13 +0200

    random idea

--- lab3/z1.py
+++ lab3/z1.py
@@ -1,4 +1,6 @@
 import string
+import re
+import math
 def lacz(s1,s2):
     res = ""
     lr =0
@@ -115,12 +117,132 @@ def onp(w):
         w = w[1:-1]
     p = bal(w, ">")
     if p >=0:
-        return w[p] + onp(w[:p]) +  onp(w[p+1:])
+        return onp(w[:p]) +  onp(w[p+1:]) + w[p]
     p = bal(w, "|&")
     if p >=0:
-        return w[p] + onp(w[:p]) +  onp(w [p+1:])
+        return onp(w[:p]) +  onp(w[p+1:]) + w[p]
+    if w[0]=="~":
+        return w[1]+w[0]+w[2:]
     return w
+#---------------------------------------------------------
+def listelements(dane):
+    r = []
+    tmp = ""
+    i =0
+    for el in dane:
+        if(el != "(" and el != ")" and el != "|"):
+            tmp += el
+        if(el == ")"):
+            r.append(tmp)
+            tmp = ""
+    return r
+def eq(str1, str2):
+    lit1 = [a for a in str1 if a in VAR]
+    lit2 = [a for a in str2 if a in VAR]
+    if len(lit1) != len(lit2):
+        return False
+    k = [True for (a,b) in zip(lit1, lit2) if a ==b]
+    if len(k)==len(lit1):
+        return True
+    return False
+def connect(listd):
+    tmp = [" "]
+    tmp[0]= "("+listd[0]+")"
+    r = [tmp]
+    for x in range(1, len(listd)):
+        flag = True
+        for y in range(0, len(r)):
+            if(eq(listd[x], r[y][0])):
+                r[y].append("("+listd[x]+")")
+                flag = False
+        if(flag):
+            tmp = [""]
+            tmp[0] = listd[x]
+            r.append(tmp)
+    return r
 
+def maskuj2(mask, orginal):
+    t =""
+    for m,e in zip(mask,orginal):
+        if m == '1':
+            t += e + "|"
+    return t[:-1]
+def dopelninie(mask, orginal):
+    t =[]
+    for m,e in zip(mask,orginal):
+        if m == '0':
+            t.append(e)
+    return t
+def var(w):
+    return "".join(sorted(set(w) & set(VAR)))
+
+def mapuj3(w,zm, val):
+    l = list(w)
+    for i in range(len(l)):
+        p = zm.find(l[i])
+        if p >= 0: l[i] = val[p]
+    return "".join(l)
+
+def Or(a,b): return a or b
+def And(a,b): return a and b
+def Imp(a,b): return not a or b
+def value(w,val):
+    v = var(w)
+    w = mapuj3(w,v,val)
+    st = []
+    for z in w:
+        if z in "01": st.append(int(z))
+        elif z=="|": st.append(Or(st.pop(),st.pop()))
+        elif z=="&": st.append(And(st.pop(),st.pop()))
+        elif z==">": st.append(Imp(st.pop(),st.pop()))
+        elif z=="~": st.append(int(not st.pop()))
+    return st[0]
+def gen(n):
+    for i in range(2**n):
+        yield bin(i)[2:].rjust(n, "0")
+sources = {
+    '0110': "0^1",
+    '01101001': "0^1^2"
+}
+def shorten(s):
+    v = var(s)
+    result =s
+    r = ""
+    lines = gen(len(v))
+    for l in lines:
+        r += str((value(onp(s), l)))
+    #print(r)
+    if r in sources:
+        result  = sources[r]
+        variables = "".join(sorted(set(s) & set(VAR)))
+        r2=""
+        for e in result:
+            if e in "0123456789":
+                r2 += variables[int(e)]
+            else:
+                r2 += e
+        result = r2
+    return result
+def check(lista):
+    if len(lista) == 0:
+        return ""
+    res_len = math.inf
+    masks  = [str(bin(i)[2:].zfill(len(lista))) for i in range(1, 2**len(lista), 1)]
+    #print(masks)
+    for m in masks:
+        #print(maskuj2(m,lista),  dopelninie(m, lista))
+        tmp1 = shorten(maskuj2(m,lista))
+        tmp2 = check(dopelninie(m, lista))
+        if(tmp2 != ""):
+            tmp = tmp1 +"|"+tmp2
+        else:
+            tmp = tmp1
+        if min(len(tmp), res_len):
+            res = tmp
+            res_len = len(res)
+    return res
+
+#---------------------------------------------------------
 if  __name__ == "__main__" :
     f =open("xor")
     dane = set(f.read().split()) # defaultowo split przyjmuje białe znaki i dodatkowo łączy wiele spacji #EMEJZING *o*
@@ -129,5 +251,9 @@ if  __name__ == "__main__" :
     dane = redukuj(dane)
     dane = redukuj2(dane, orginal)
     dane = wypisz(dane)
-    dane = onp(dane)
-    print(dane)
+    #print(dane)
+    k = connect(listelements(dane))
+    k = check(k[0])
+    print(k)
+    #dane = onp(dane)
+    #print(dane)
